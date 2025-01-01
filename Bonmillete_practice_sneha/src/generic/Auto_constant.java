4309223ab638b38761cd42bd269fb4895d61dddd
sneha758 <sneha456tr@gmail.com>
2025-01-01 19:53:12 +0530

    Auto_constants added

---
+++ Bonmillete_practice_sneha/src/generic/Auto_constant.java
@@ -0,0 +1,12 @@
+package generic;
+
+public interface Auto_constant 
+{
+  String urlOfApplication="http://localhost:5173";
+  
+  String rootFolder = System.getProperty("user.dir");
+  
+  String pathOfExcel=rootFolder +"/*projectDocuments";
+  
+  String screenshotFolder=rootFolder +"/failedScreenshots";
+}
